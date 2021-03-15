b16c12814e93eee6008d30a693b43143f05d0cfd
ntheodorg <73588948+ntheodorg@users.noreply.github.com>
2021-03-15 15:59:12 +0200

    fixed

--- logics/Headers/joc.h
+++ logics/Headers/joc.h
@@ -1,6 +1,10 @@
 #ifndef JOC_H_INCLUDED
 #define JOC_H_INCLUDED
 
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+
 #define MAX_STRATURI 3
 
 struct strat
@@ -10,16 +14,15 @@ struct strat
 
 struct om
 {
-    int x, y;
+    int x, y, W, H;
 };
 
 struct obstacol
 {
-    float x,y,speed;
-    int W,H;
+    int x, y, W, H;
 };
 
-void crearePlansa(struct strat* arr, struct obstacol* lemne)
+void crearePlansa(struct strat* arr, struct obstacol& lemne)
 {
     arr[1].x=getmaxx()/2;
     arr[1].y=getmaxy()-100;
@@ -27,6 +30,10 @@ void crearePlansa(struct strat* arr, struct obstacol* lemne)
     arr[0].y=arr[1].y;
     arr[2].x=(getmaxx()+arr[1].x)/2;
     arr[2].y=arr[1].y;
+    lemne.H=20;
+    lemne.W=100;
+    lemne.x=arr[1].x;
+    lemne.y=0-lemne.H;
 }
 
 void fata(int x, int y)
@@ -66,28 +73,82 @@ void animatieOm(om chaser, int x, float &time)
     }
 }
 
-void obstacol(int x, int y)
+void deseneaza(struct obstacol &lemne,int x, int y)
 {
-    int W=30,H=20;
+    int W=lemne.W,H=lemne.H;
     rectangle(x-W/2,y-H/2,x+W/2,y+H/2);
 }
 
-void generator(strat* arr, float & time)
+obstacol* generator(strat* arr,obstacol& lemne)
 {
+    srand(time(NULL));
+    obstacol* vect=new obstacol[MAX_STRATURI];
+    int numarGenerat;
+    for(int i=0;i<MAX_STRATURI;i++) //copiez marimea si schimb coordonatele
+    {
+        numarGenerat=rand()%100+0;
+        if(numarGenerat%2==0)
+        {
+            vect[i].H=lemne.H;
+            vect[i].W=lemne.W;
+            vect[i].x=arr[i].x;
+            vect[i].y=vect[i].H;
+        }
+        else
+        {
+            vect[i].H=0;
+            vect[i].W=0;
+            vect[i].x=0;
+            vect[i].y=0;
+        }
+    }
+    char ceva[1000];
+    /*sprintf(ceva,"%d %d %d",vect[0].x,vect[1].x,vect[2].x);
+    outtextxy(400,200,ceva);
+    sprintf(ceva,"%d %d %d",vect[0].y,vect[1].y,vect[2].y);
+    outtextxy(400,300,ceva);*/
+    return vect;
+}
+bool ok=0;
+void obstacole(strat* arr, float time, struct obstacol &lemne)
+{
+    obstacol* obs;
+    int speed=1;
+    if(1)//&& time>10)
+        ok=1,obs=generator(arr,lemne);
+
+    char ceva[1000];
+    sprintf(ceva,"%d %d %d",obs[0].x,obs[1].x,obs[2].x);
+    outtextxy(400,400,ceva);
+    sprintf(ceva,"%d %d %d",obs[0].y,obs[1].y,obs[2].y);
+    outtextxy(400,500,ceva);
+
+    for(int i=0;i<MAX_STRATURI;i++)
+    {
+        //obs[i].y+=speed;
+        if(obs[i].W!=0 && obs[i].H!=0)
+            deseneaza(lemne,obs[i].x+i*2,obs[i].y+=2);
+    }
+    //lemne.x
+    //daca lemnele au ajuns jos delete
+
 
 }
 
 void joc()
 {
+    setbkcolor(BGColor);
+    setcolor(PColor);
+
     struct om chaser;
     chaser.y=getmaxy()-100;
 
     struct obstacol lemne;
 
-    setbkcolor(BGColor);
-    setcolor(PColor);
-    strat straturi[3];
-    crearePlansa(straturi,&lemne);
+    strat straturi[MAX_STRATURI];
+
+    crearePlansa(straturi,lemne);
+
     int page=3,controller=1,pauza=0;
     float globalTime=0,animTime=0; //ANIMTIME E DOAR PENTRU OM DEOCAMDATA
     //animatieOm(straturi[1].x,straturi[1].y); //press any key to start
@@ -99,8 +160,7 @@ void joc()
 
         outtextxy(200,200,"ciorba");
 
-        //generator(straturi,globalTime);//,lemne); //OBSTACOLELE
-
+        obstacole(straturi,globalTime,lemne); //OBSTACOLELE
 
         if(!pauza)
         {
